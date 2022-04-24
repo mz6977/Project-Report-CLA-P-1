b8b8ad63f1fc270cf1bc510c4b5ecdbb7d02cae1
mz6977 <103304263+mz6977@users.noreply.github.com>
2022-04-24 21:55:24 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+print("Please enter two numbers");
+scanf("%d %d",&a,&b);
+printf("Sum Of Numbers= %d\n",a+b);
+printf("Difference Of Numbers= %d\n",a-b);
+printf("Product Of Numbers= %d\n",a*b);
+printf("Division Of Numbers= %d\n",a/b);
+printf("Remainder Of Numbers= %d\n",a%b);
+}
