0a6e0caa73df03aedefda39461cf0870f7bc18cc
mz6977 <103304263+mz6977@users.noreply.github.com>
2022-04-28 12:45:35 +0530

    Update first.c

--- first.c
+++ first.c
@@ -2,11 +2,12 @@
 int main()
 {
 int a,b;
-print("Please enter two numbers");
+printf("Please enter two numbers");
 scanf("%d %d",&a,&b);
 printf("Sum Of Numbers= %d\n",a+b);
 printf("Difference Of Numbers= %d\n",a-b);
 printf("Product Of Numbers= %d\n",a*b);
 printf("Division Of Numbers= %d\n",a/b);
 printf("Remainder Of Numbers= %d\n",a%b);
+return 0;
 }
